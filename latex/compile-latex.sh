0bec549224e7f0d649c6bd2190c5f4c8f887a5e0
tarikgit <tarikocaktan@gmail.com>
2020-03-07 00:46:53 +0100

    Initial commit

--- latex/compile-latex.sh
+++ latex/compile-latex.sh
@@ -3,10 +3,15 @@
 # We have to make it an executable file by changing the permissions.
 # chmod u+x compile-latex.sh
 
-latex $1".tex" &
-latex $1".tex" &
-latex $1".tex" &
+latex -shell-escape $1".tex" &
+latex -shell-escape $1".tex" &
+latex -shell-escape $1".tex" &
 dvips $1".dvi" &
 
-#ps2pdf $1".ps"
+ps2pdf $1".ps"
 #evince $1".pdf" &
+
+#mv *.aux files/
+#mv *.dvi files/
+#mv *.log fles/
+#mv *.ps files/
