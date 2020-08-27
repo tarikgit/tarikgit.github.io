979197142db64fc7283f4ef1fd844a47f748da13
tarikgit <tarikocaktan@gmail.com>
2020-08-27 21:55:57 +0200

    latex template for ecoai

---
+++ latex/2020-2021/02-ECOAI/template/mycompile.sh
@@ -0,0 +1,13 @@
+#latex ocaktan_curriculum_oct2011_jobmarket
+#latex ocaktan_curriculum_oct2011_jobmarket
+#latex ocaktan_curriculum_oct2011_jobmarket
+#dvips ocaktan_curriculum_oct2011_jobmarket.dvi
+#ps2pdf ocaktan_curriculum_oct2011_jobmarket.ps
+#acroread ocaktan_curriculum_oct2011_jobmarket.pdf &
+
+latex slides
+latex slides
+latex slides
+dvips slides.dvi
+ps2pdf slides.ps
+#acroread slides.pdf &
